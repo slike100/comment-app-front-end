fbd398adfbaadb5ffea270fd4b4a2c954e1987ed
Sam Slike <samslike@Techtonics-MacBook-Pro.local>
2019-04-05 12:40:51 -0600

    added in the indexdb functionality

--- app.js
+++ app.js
@@ -3,64 +3,67 @@
 //   fetchComments(e);
 // })
 
-(function fetchComments(event) {
-  fetch('https://us-central1-fir-cb-backend.cloudfunctions.net/api/comment/')
-  .then(res => res.json())
-  .then(data => creatingComments(data));
-  // setTimeout(fetchComments, 1000);
-})();
+// function fetchComments(event) {
+//   fetch('https://us-central1-fir-cb-backend.cloudfunctions.net/api/comment/')
+//   .then(res => res.json())
+//   .then(data => creatingComments(data));
+//   // setTimeout(fetchComments, 1000);
+// };
 
-function creatingComments(data){
-  var divComments = document.querySelector('.comments');
-  divComments.innerHTML = '';
-  for (var i = 0; i < data.length; i++) {
-    var divHolder = document.createElement('div');
-    var list = document.createElement('ul');
+// function creatingComments(data){
+//   var divComments = document.querySelector('.comments');
+//   divComments.innerHTML = '';
+//   for (var i = 0; i < data.length; i++) {
+//     var divHolder = document.createElement('div');
+//     var list = document.createElement('ul');
+//
+//     var listAuthor = document.createElement('li');
+//     listAuthor.innerText = data[i].author;
+//     listAuthor.dataset.author = data[i].author;
+//     list.append(listAuthor);
+//
+//     var listComment = document.createElement('li');
+//     listComment.innerText = data[i].comment;
+//     listComment.dataset.comment = data[i].comment;
+//     list.append(listComment);
+//
+//     divHolder.append(list);
+//     divHolder.addClassName = "smallComments";
+//
+//     var deleteBtn = document.createElement('button');
+//     deleteBtn.innerText = 'Delete this comment?';
+//     deleteBtn.dataset._id = data[i]._id;
+//     deleteBtn.addEventListener('click', function(e){
+//       e.preventDefault();
+//       deleteData(e.target.dataset._id);
+//     })
+//     divHolder.append(deleteBtn);
+//
+//     divComments.append(divHolder);
+//   }
+// }
 
-    var listAuthor = document.createElement('li');
-    listAuthor.innerText = data[i].author;
-    listAuthor.dataset.author = data[i].author;
-    list.append(listAuthor);
 
-    var listComment = document.createElement('li');
-    listComment.innerText = data[i].comment;
-    listComment.dataset.comment = data[i].comment;
-    list.append(listComment);
-
-    divHolder.append(list);
-    divHolder.addClassName = "smallComments"
-
-    var deleteBtn = document.createElement('button');
-    deleteBtn.innerText = 'Delete this comment?';
-    deleteBtn.dataset._id = data[i]._id;
-    deleteBtn.addEventListener('click', function(e){
-      e.preventDefault();
-      console.log(e.target.dataset._id);
-      deleteData(e.target.dataset._id);
-    })
-    divHolder.append(deleteBtn);
-
-    divComments.append(divHolder);
-  }
-}
-
-
-document.querySelector('form').addEventListener('submit', function(e){
-  e.preventDefault();
-  var myObj = {}
-  for (var i = 0; i < e.target.length-1; i++) {
-    myObj[e.target[i].name] = e.target[i].value;
-  }
-  console.log(myObj);
-  postData(`https://us-central1-fir-cb-backend.cloudfunctions.net/api/comment/`, myObj);
-})
+// document.querySelector('form').addEventListener('submit', function(e){
+//   e.preventDefault();
+//   var myObj = {}
+//   for (var i = 0; i < e.target.length-1; i++) {
+//     console.log(typeof e.target[i].name);
+//     if(e.target[i].name == false || e.target[i].value == false){
+//       alert('Name and comment must include something');
+//     } else {
+//       myObj[e.target[i].name] = e.target[i].value;
+//     }
+//   }
+//   console.log(myObj);
+//   postData(`https://us-central1-fir-cb-backend.cloudfunctions.net/api/comment/`, myObj);
+// })
 
 
 function postData(url = `https://us-central1-fir-cb-backend.cloudfunctions.net/api/comment/`, data = {}) {
   console.log('help');
     return fetch(url, {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, cors, *same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
         credentials: "same-origin", // include, *same-origin, omit
         headers: {
@@ -72,6 +75,8 @@ function postData(url = `https://us-central1-fir-cb-backend.cloudfunctions.net/a
     })
     .then(function(response){
       response.json();
+      // fetchComments();
+      // document.querySelector('form').reset();
     })
 }
 
@@ -82,5 +87,9 @@ function deleteData(item) {
   })
   .then(function(response){
     console.log(response);
+    // fetchComments();
+  })
+  .catch(function (error) {
+        return;
   });
 }
